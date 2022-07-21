57b22b90ab3115d48d8d06dc6879409b318a15c5
Lany Aragão <lanyaragao5@gmail.com>
2022-07-21 19:09:52 -0300

    Projeto MVC - testando lista em view

---
+++ ProjetoProdutos/ProjetoProdutos/ProjetoProdutos/Controllers/ProdutoController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoProdutos.Models;
+
+namespace ProjetoProdutos.Controllers
+{
+    public class ProdutoController: Controller
+    {
+        static List<Produto> lista = new List<Produto>();
+        public IActionResult Index()
+        {
+            lista.Add(new Produto() { NomeProduto = "Mouse", Categoria = "Informatica", Preco = 55.55, Quantidade = 23, Descricao = "Sem fio" });
+            ViewBag.Produto = lista;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Produto prod)
+        {
+            lista.Add(prod);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Atualizar(int? Id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Atualizar(int? id, Produto prod)
+        {
+            Produto produtoAlterado = new Produto();
+            int i = 0;
+            foreach (var item in lista)
+            {
+                if (item.Id == id)
+                {
+                    item.NomeProduto = prod.NomeProduto;
+                    item.Descricao = prod.Descricao;
+                    item.Categoria = prod.Categoria;
+                    item.Quantidade = prod.Quantidade;
+                    item.Preco = prod.Preco;
+                    lista[i] = item;
+                    break;
+                }
+                i++;
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar(int? Id)
+        {
+            Produto produto = new Produto();
+            {
+                int i = 0;
+                foreach(var item in lista)
+                {
+                    if (item.Id == produto.Id)
+                    {
+                        lista.RemoveAt(i);
+                    }
+                    i++;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ ProjetoProdutos/ProjetoProdutos/ProjetoProdutos/Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjetoProdutos.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string NomeProduto { get; set; }
+        public string Descricao { get; set; }
+        public string Categoria { get; set; }
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
+        public Produto() { }
+        public Produto(int id, string nomeProduto, string descricao, string categoria, int quantidade, double preco)
+        {
+            Id = id;
+            NomeProduto = nomeProduto;
+            Descricao = descricao;
+            Categoria = categoria;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+    }
+}
