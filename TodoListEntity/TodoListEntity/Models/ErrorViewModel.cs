e0e37586c5cbec88c56394e2f2c2b7e3d2e3b3ff
Lany Aragão <lanyaragao5@gmail.com>
2022-07-22 17:35:00 -0300

    To-do list with Bootstrap, Razor, CSS

---
+++ TodoListEntity/TodoListEntity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoListEntity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
