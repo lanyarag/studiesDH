e8f7f97ea69ee1c96c7b03581efca1df2f2ea989
Lany Aragão <lanyaragao5@gmail.com>
2022-07-22 21:13:34 -0300

    Student App - MVC, Bootstrap, Razor, HTML

---
+++ StudentApp/StudentApp/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApp.Models;
+
+namespace StudentApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public List<Student> list = new List<Student>();
+        public IActionResult Index()
+        {
+            list.Add(new Student() {Id = 1, Name = "Willian", LastName="Sant Anna", Status= "Disapproved"}); 
+            list.Add(new Student() {Id = 1, Name = "Thiago", LastName="Machado", Status= "Approved"});
+            list.Add(new Student() {Id = 1, Name = "Caroline", LastName="Da Silva", Status= "Recuperation"});
+            ViewBag.Student = list;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddStudent()
+        {
+            list.Add(new Student());
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StudentApp/StudentApp/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Status { get; set; }
+    }
+}
